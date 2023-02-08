2d6ebe3ef26e4b5d26fbb6538109464c0a8ff69e
Abhay Shah <abhayshahdev@gmail.com>
2023-02-08 12:20:54 +0530

    Add project files.

---
+++ DAL/DataAccessLayer.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebDevRequirementsExamples.Domain;
+
+namespace WebDevRequirementsExamples.DAL
+{
+    public class DataAccessLayer
+    {
+        static string conStr = ConfigurationManager.AppSettings.Get("ConString").ToString();
+        public static SqlConnection GetConnection()
+        {
+            var conn = new SqlConnection(conStr);
+            conn.Open();
+            return conn;
+        }
+
+        public static List<customers> FetchList()
+        {
+            string sql = "SELECT TOP(5) [city] as City, COUNT(customer_id) as TotalCustomers FROM [BikeStores].[sales].[customers] GROUP BY [state], city";
+            using(var con = GetConnection())
+            {
+                //using dapper
+                return con.Query<customers>(sql).ToList();
+            }
+        }
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDevRequirementsExamples
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var list = DAL.DataAccessLayer.FetchList();
+
+            if(list.Count > 0)
+            {
+                string chartData = "";
+                string view = "";
+                string labels = "";
+
+                chartData += "<script>";
+                foreach(var item in list)
+                {
+                    view += item.TotalCustomers + ",";
+                    labels += "\"" + item.City + "\",";
+                }
+
+                view = view.Substring(0, view.Length - 1);
+                labels = labels.Substring(0, labels.Length - 1);
+
+                chartData += " chartLabels = [" + labels + "]; chartData = [" + view + "]; ";
+                chartData += "</script>";
+
+                ltChartData.Text = chartData;
+            }
+        }
+    }
+}
